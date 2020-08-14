0fe2371d19277e2b1457d8c4996e9b3875a5d766
corey2915 <coreyc538@gmail.com>
2020-08-13 20:58:54 -0400

    First commit

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-function setup() {
-  let inp = createInput('');
-   inp.input(intputAnNumber);
+n = 13
+k = 2
+function equation(n, k) {
+    if (n == 1) {
+        return 1
+    } else {
+        return (equation(n - 1, k) + k - 1) % n + 1
+    }
 }
-
-function draw() {
-
-}
-
-function intputAnNumber(){
-var number = this.value();
-console.log(number);
-
-}
+console.log("The chosen place is ", equation(n, k))
